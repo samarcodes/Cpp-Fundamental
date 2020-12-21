ddcc270ed7c98d1d545e7d018feb22ad08855d64
Samar Kalra <47415857+samarcodes@users.noreply.github.com>
2020-12-21 15:09:41 +0530

    Update read.cpp
    basic ouput formatting

--- OOPS/7file handling/read.cpp
+++ OOPS/7file handling/read.cpp
@@ -8,14 +8,15 @@ int main() {
 	fin.open("myfile.dat");
 	//reading
 	char ch;
-	cout << fin.tellg(); //0 coz no character is read till now
+	cout << fin.tellg() << endl; //0 coz no character is read till now
 	fin.get(ch);
-	cout << fin.tellg(); //1 coz one character is read
+	cout << fin.tellg() << endl; //1 coz one character is read
 	while (fin) {
 		cout << ch;
 		fin.get(ch);
 	}
-	cout << fin.tellg(); //-1 as all character are read
+	cout << endl;
+	cout << fin.tellg() << endl; //-1 as all character are read
 
 	fin.close();
 
